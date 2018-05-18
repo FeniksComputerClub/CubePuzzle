ccd02a40b8705c11abb9ddc2a082d89c09ccbeab
sjaak31367 <sjaak31367@gmail.com>
2018-05-18 14:42:29 +0200

    W.I.P.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ HEADERS = Direction.h Piece.h PiecePosition.h PositionSet.h
 SOURCES = Direction.cxx PositionSet.cxx Piece.cxx main.cxx
 
 cube: $(HEADERS) $(SOURCES)
-	g++ -std=c++11 -g $(SOURCES) -o cube
+	g++ -std=c++14 -g $(SOURCES) -o cube
 
 tags ctags:
 	ctags $(HEADERS) $(SOURCES)
--- Piece.cxx
+++ Piece.cxx
@@ -2,10 +2,24 @@
 #include <iostream>
 
 //static
-uint64_t const Piece::s_pieces[13] = { R1, R2, R3, R4, Y1, Y2, Y3, Y4, Y5, B1, B2, B3, B4 };
+std::array<Piece, 13> const Pieces::s_pieces = { R1, R2, R3, R4, Y1, Y2, Y3, Y4, Y5, B1, B2, B3, B4 };
 
 std::ostream& operator<<(std::ostream& os, Piece const& piece)
 {
   os << static_cast<PositionSet const&>(piece);
   return os;
 }
+
+Piece::Piece(int nr) : PositionSet(Pieces::instance().get(nr))
+{
+}
+
+//static
+Pieces& Pieces::instance()
+{
+  if (s_pieces_instance == nullptr)
+    s_pieces_instance = new Pieces;
+  return *s_pieces_instance;
+}
+
+Pieces* Pieces::s_pieces_instance;
